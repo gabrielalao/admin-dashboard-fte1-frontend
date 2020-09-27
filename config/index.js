968439e1e1c71f4647d5201a4bfec8e7b45749fe
Gabriel Alao <gabrieldeveloper1@gmail.com>
2020-09-27 15:57:08 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    SERVER_API_LOCATION: 'https://cfaffiliate360-adminpanel.herokuapp.com:4000/',
+    SERVER_API_LOCATION: 'https://cfaffiliate360-adminpanel.herokuapp.com/',
     LOCAL_API_LOCATION: 'http://localhost:4000/',
     SOCKET_ADDR: 'localhost:3000',
 }
